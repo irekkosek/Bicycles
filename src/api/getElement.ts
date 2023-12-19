31d7cc38ef85813f452dd200c2d2bcdc6874c435
Graziaczek <66276396+Graziaczek@users.noreply.github.com>
2023-12-19 01:07:38 +0100

    Fetching url v1

---
+++ src/api/getElement.ts
@@ -0,0 +1,24 @@
+import { ConfigEnv } from "./env.config";
+
+export const fetchElement = async ( name: String, fields: string, limit: number) => {
+    const url = `https://api.cyclestreets.net/v2/geocoder?key=${ConfigEnv.apiKey}&q=${name}&limit=${limit}&fields=${fields}`;
+    console.log(url);
+    // const response = await fetch(url)
+    // await response.json().
+    // then((data) => {
+    //     console.log(JSON.parse(data).features[0]);
+    // return data.features[0];
+    // }).catch((error) => {
+    //     console.log(error);
+    // }
+    // );
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data);
+        console.log(data.features[0]);
+        return data.features;
+    }
+    ).catch((error) => {
+        console.log(error);
+    }
+    );
+};
--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,8 +1,60 @@
 <script setup lang="ts">
 import { TheDestinationPicker, TheTripPicker } from "../components";
+<<<<<<< Updated upstream
 import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
 
 const zoom = 15;
+=======
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+//import L from "leaflet";
+//import { fetchPOI } from "../api";
+import { fetchElement } from "../api/getElement";
+
+const zoom = 15;
+
+const isTripPickerVisible = ref(false);
+
+const map = ref(null)
+
+// var map = L.map('map').setView([42.35, -71.08], 3);
+
+// // load a tile layer
+// L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
+//   {
+//     attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
+//     maxZoom: 17,
+//     minZoom: 9
+//   }).addTo(map);
+
+// // load GeoJSON from an external file
+// fetch("countries.geojson").then(res => res.json()).then(data => {
+//   // add GeoJSON layer to the map once the file is loaded
+//   L.geoJson(data).addTo(map);
+// });
+
+/*const test = async () => {
+
+  const map = L.map('map');
+  const lat = 50.29117904070245;
+  const lon = 18.680356029431803;
+  const radius = 1;
+  const limit = 10;
+  const data = await fetchPOI(lat, lon, radius, limit)
+  L.geoJson(data).addTo(map);
+  console.log(map)
+}*/
+const test2 = async () => {
+  const name = "Rydułtowy";
+  const fields = "name";
+  const limit = 10;
+  const data = await fetchElement(name, fields, limit)
+  console.log(data)
+}
+test2()
+watch(() => isTripPickerVisible.value, () => {
+  //test()
+})
+>>>>>>> Stashed changes
 </script>
 
 <template>
