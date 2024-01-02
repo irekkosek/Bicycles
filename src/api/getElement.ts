920eb1996c9fdb41ba31f5b0efac74e1a6c970ae
Grażyna Wyrtki <66276396+Graziaczek@users.noreply.github.com>
2024-01-02 18:06:57 +0100

    Update getElement.ts

--- src/api/getElement.ts
+++ src/api/getElement.ts
@@ -3,8 +3,6 @@ import { ConfigEnv } from "./env.config";
 export const fetchElement = async ( name: String, fields: string, limit: number) => {
     const url = `https://api.cyclestreets.net/v2/geocoder?key=${ConfigEnv.apiKey}&q=${name}&limit=${limit}&fields=${fields}`;
     return(fetch(url).then(res => res.json()).then(data => {
-        console.log(data);
-        console.log(data.features[0]);
         return data.features;
     }
     ).catch((error) => {
