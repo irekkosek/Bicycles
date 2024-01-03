54abdaab02c7b7bc80158b28f77c95b268e83750
Graziaczek <66276396+Graziaczek@users.noreply.github.com>
2024-01-04 00:29:25 +0100

    namechange

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,42 +1,12 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { TheDestinationPicker, TheTripPicker } from "../components";
-import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
-import { fetchElement } from "../api/getElement";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
 
 const zoom = 15;
 
-//const isTripPickerVisible = ref(false);
+const isTripPickerVisible = ref(false);
 
-//const map = ref(null)
-
-// var map = L.map('map').setView([42.35, -71.08], 3);
-
-// // load a tile layer
-// L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
-//   {
-//     attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
-//     maxZoom: 17,
-//     minZoom: 9
-//   }).addTo(map);
-
-// // load GeoJSON from an external file
-// fetch("countries.geojson").then(res => res.json()).then(data => {
-//   // add GeoJSON layer to the map once the file is loaded
-//   L.geoJson(data).addTo(map);
-// });
-
-/*const test = async () => {
-
-  const map = L.map('map');
-  const lat = 50.29117904070245;
-  const lon = 18.680356029431803;
-  const radius = 1;
-  const limit = 10;
-  const data = await fetchPOI(lat, lon, radius, limit)
-  L.geoJson(data).addTo(map);
-  console.log(map)
-}*/
 
 </script>
 
